1c02a6bcec179a019062e498362e3aa5a01005b0
pouria <pouria.sameti80@gmail.com>
2023-12-20 13:56:19 +0330

    random epsilon greedy policy implementation.

--- code/main.py
+++ code/main.py
@@ -1,33 +1,52 @@
 import gym
 import gym_maze
 import time
-
-# Create an environment
-
-env = gym.make("maze-random-10x10-plus-v0")
-observation = env.reset()
-
-# Define the maximum number of iterations
-NUM_EPISODES = 1000
-
-for episode in range(NUM_EPISODES):
-
-    # Note: .sample() is used to sample random action from the environment's action space
-
-    # Choose an action (Replace this random action with your agent's policy)
-    action = env.action_space.sample()
-
-    # Perform the action and receive feedback from the environment
-    next_state, reward, done, truncated = env.step(action)
-    print(next_state, reward, done, truncated)
-
-    if done or truncated:
-        observation = env.reset()
-
-    env.render()
-    time.sleep(0.1)
+import itertools
+import numpy as np
 
 
-# Close the environment
-env.close()
+def random_epsilon_greedy_policy(Q: np.ndarray, state: int, epsilon: float):
+    portoghal = np.random.randint(0, 100) / 100
+    actions = list(range(env.action_space.n))
+    if portoghal < epsilon:
+        return np.random.choice(actions)
+    else:
+        return np.argmax(Q[state])
 
+
+def mapping(observation: tuple):
+    return int(observation[0] * 10 + observation[1])
+
+
+if __name__ == '__main__':
+
+    # Create an environment
+    env = gym.make("maze-random-10x10-plus-v0")
+    observation = env.reset()
+
+    nS = 100
+    nA = 4
+    NUM_EPISODES = 1000
+    epsilon = 0.4
+    Q = np.zeros([nS, nA])
+    state = observation
+
+    for episode in range(NUM_EPISODES):
+        for t in itertools.count():
+            action = random_epsilon_greedy_policy(Q, mapping(state), epsilon)
+            # Note: .sample() is used to sample random action from the environment's action space
+
+            # Perform the action and receive feedback from the environment
+            next_state, reward, done, truncated = env.step(action)
+            # print(next_state, reward, done, truncated)
+
+            epsilon -= 0.01
+            if done or truncated:
+                observation = env.reset()
+                state = observation
+
+        env.render()
+        time.sleep(0.1)
+
+    # Close the environment
+    env.close()
