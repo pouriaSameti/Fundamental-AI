bc79266d0d393aed8ee19291e169b55f39b8df79
pouria <pouria.sameti80@gmail.com>
2023-12-24 09:57:12 +0330

    RL => model_initialization() implementation.

--- code/RL.py
+++ code/RL.py
@@ -4,10 +4,20 @@ from collections import deque
 
 
 class DeepQLearning:
-    def __init__(self, batch_size: int, memory_length: int):
+    def __init__(self, n_states:int, n_actions: int, batch_size: int, memory_length: int,
+                 neuron_per_layer: int):
+
         self.batch_size = batch_size
-        self.memory_len = memory_length
+        self.memory = deque(maxlen=memory_length)
+        self.model = DeepQLearning.__model_initialization(neuron_per_layer=neuron_per_layer, n_states=n_states,
+                                                          n_actions=n_actions)
 
-        self.memory = deque(maxlen=self.memory_len)
-        self.model = None
-
+    @classmethod
+    def __model_initialization(cls, neuron_per_layer: int, n_states: int, n_actions: int):
+        model = keras.Sequential([
+            keras.layers.Dense(neuron_per_layer, activation='elu', input_shape=n_states),
+            keras.layers.Dense(neuron_per_layer, activation='elu'),
+            keras.layers.Dense(neuron_per_layer, activation='elu'),
+            keras.layers.Dense(n_actions)
+        ])
+        return model
