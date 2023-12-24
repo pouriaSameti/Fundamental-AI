b8a72964513d90a258f7d6bc937c76237739e738
pouria <pouria.sameti80@gmail.com>
2023-12-24 09:43:16 +0330

    RL => DeppQLearning initialization.

--- code/RL.py
+++ code/RL.py
@@ -1,2 +1,13 @@
+import keras
+import tensorflow as tf
+from collections import deque
+
+
 class DeepQLearning:
-    pass
+    def __init__(self, batch_size: int, memory_length: int):
+        self.batch_size = batch_size
+        self.memory_len = memory_length
+
+        self.memory = deque(maxlen=self.memory_len)
+        self.model = None
+
