0e4a3144a003f4e3d06e57bcbc7037f9e4351193
pouria <pouria.sameti80@gmail.com>
2023-12-24 10:30:39 +0330

    RL => sampling_from_memory() was changed.

--- code/RL.py
+++ code/RL.py
@@ -5,11 +5,12 @@ from collections import deque
 
 
 class DeepQLearning:
-    def __init__(self, n_states: int, n_actions: int, batch_size: int, memory_length: int,
+    def __init__(self, discount_factor: float, n_states: int, n_actions: int, batch_size: int, memory_length: int,
                  neuron_per_layer: int):
 
         self.batch_size = batch_size
         self.memory = deque(maxlen=memory_length)
+        self.discount_factor = discount_factor
         self.model = DeepQLearning.__model_initialization(neuron_per_layer=neuron_per_layer, n_states=n_states,
                                                           n_actions=n_actions)
 
@@ -17,9 +18,9 @@ class DeepQLearning:
         random_indices = np.random.randint(len(self.memory), size=self.batch_size)
         batch = [self.memory[index] for index in random_indices]
 
-        next_states, rewards, dones, _ = [np.array([observation[field_index] for observation in batch])
+        states, actions, rewards, next_states, dones = [np.array([observation[field_index] for observation in batch])
                                           for field_index in range(4)]
-        return next_states, rewards, dones, _
+        return states, actions, rewards, next_states, dones
 
     @classmethod
     def __model_initialization(cls, neuron_per_layer: int, n_states: int, n_actions: int):
